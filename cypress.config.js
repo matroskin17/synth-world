65e311e536fea44b9c51ced400ed81516b92362c
Dadushka <moiobk@gmail.com>
2024-03-12 15:44:54 +0300

    Integration cypress to github

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,8 @@
 const { defineConfig } = require("cypress");
 const defaultConfig = require('./cypress/support/helpmates/defaultConfig')
 
-
-
 module.exports = defineConfig({
+  projectId: 'do7k2s',
   watchForFileChanges: false,
   ...defaultConfig.macbook,
   ...defaultConfig.timeouts,
